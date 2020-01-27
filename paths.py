a90f96af3a4ba3a496447311baeff1b7c0f62906
Chih-Hung Hsieh <chh@google.com>
2020-01-26 21:22:49 -0800

    Switch from r370808 to r377782
    Bug: 145916209
    Test: build.py
    Change-Id: I847efa7f32d247870dfafe57f3d75c5e90d43d93

--- paths.py
+++ paths.py
@@ -19,7 +19,7 @@ import build_platform
 THIS_DIR = os.path.realpath(os.path.dirname(__file__))
 
 STAGE0_RUST_VERSION = '1.40.0'
-CLANG_REVISION = 'r370808'
+CLANG_REVISION = 'r377782'
 
 
 def workspace_path(*args):
