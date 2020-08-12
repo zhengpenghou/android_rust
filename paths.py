9b09f869241c4adfd8054cf6ceae91ddf3ef774d
Stephen Hines <srhines@google.com>
2020-08-12 02:57:36 -0700

    Update rust toolchain to build with clang-r383902b.
    Bug: http://b/149839606
    Test: Treehugger
    Change-Id: If84526a16983febb8a9d5093187348c03721c8c3

--- paths.py
+++ paths.py
@@ -19,7 +19,7 @@ import build_platform
 THIS_DIR = os.path.realpath(os.path.dirname(__file__))
 
 STAGE0_RUST_VERSION = '1.44.0'
-CLANG_REVISION = 'r383902'
+CLANG_REVISION = 'r383902b'
 
 
 def workspace_path(*args):
