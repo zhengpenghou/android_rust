f04a007d19969f2b4f9ebb34e143d5b3df604f20
Chris Wailes <chriswailes@google.com>
2021-10-12 17:25:50 -0700

    Improved handling of branching
    This CL updates fetch_source.py with some additional logic to better
    handle branching.  It will now test for the existence of a branch before
    attempting to create it.  If it exists and you have passed the
    --overwrite flag it will check out the branch and amend it.  If the
    branch exists and the flag wasn't set the script will raise an error.
    
    Test: `./fetch_source.py -b 1.56.0` with and without the branch existing
    Change-Id: I6d464e6428f837cf4e563b979c448bc6e534f119

--- fetch_source.py
+++ fetch_source.py
@@ -20,12 +20,19 @@ import subprocess
 
 from paths import RUST_SOURCE_PATH
 
+BRANCH_NAME: str = "rust-update-source-%s"
 
-COMMAND_GIT_ADD   : str = "git add ."
-COMMAND_GIT_COMMIT: str = "git commit --no-verify -m 'Importing rustc-%s'"
-COMMAND_GIT_RM    : str = "git rm -fr *"
-COMMAND_FETCH     : str = "curl --proto '=https' --tlsv1.2 -f %s | tar xz --strip-components=1"
-COMMAND_REPO      : str = "repo start rust-update-source-%s"
+COMMAND_GIT_ADD:      str = "git add ."
+COMMAND_GIT_AMEND:    str = "git commit --amend --no-edit"
+COMMAND_GIT_CHECKOUT: str = "git checkout %s"
+COMMAND_GIT_COMMIT:   str = "git commit --no-verify -m 'Importing rustc-%s'"
+COMMAND_GIT_DIFF:     str = "git diff --cached --quiet"
+COMMAND_GIT_RM:       str = "git rm -fr *"
+COMMAND_GIT_TEST:     str = "git rev-parse --verify %s"
+COMMAND_FETCH:        str = "curl --proto '=https' --tlsv1.2 -f %s | tar xz --strip-components=1"
+COMMAND_REPO:         str = "repo start %s"
+
+GIT_REFERENCE_BRANCH: str = 'aosp/master'
 
 RUSTC_SOURCE_URL_VERSION_TEMPLATE: str = "https://static.rust-lang.org/dist/rustc-%s-src.tar.gz"
 RUSTC_SOURCE_URL_BETA            : str = "https://static.rust-lang.org/dist/rustc-beta-src.tar.gz"
@@ -33,21 +40,43 @@ RUSTC_SOURCE_URL_NIGHTLY         : str = "https://static.rust-lang.org/dist/rust
 
 VERSION_PATTERN : re.Pattern = re.compile("\d+\.\d+\.\d+")
 
+branch_existed: bool = False
 
-def exec_rustc_src_command(command: str, error_string: str, stdout=subprocess.DEVNULL) -> None:
-  result = subprocess.run(command, shell=True, cwd=RUST_SOURCE_PATH, stdout=stdout)
+#
+# Command execution
+#
+
+def exec_rustc_src_command(command: str, check=False, stdout=subprocess.DEVNULL,
+                           stderr=subprocess.DEVNULL) -> subprocess.CompletedProcess:
+  return subprocess.run(command, shell=True, cwd=RUST_SOURCE_PATH, check=check,
+                        stdout=stdout, stderr=stderr)
+
+
+def handle_rustc_src_command(command: str, error_string: str, stdout=subprocess.DEVNULL,
+                             stderr=subprocess.DEVNULL) -> None:
+  result = exec_rustc_src_command(command, stdout=stdout, stderr=stderr)
   if result.returncode != 0:
     print("{}:\n{}".format(error_string, result.stderr))
     exit(-2)
 
+#
+# String operations
+#
 
-def construct_archive_url(args: argparse.ArgumentParser) -> str:
-  if args.nightly:
+def construct_archive_url(build_type: str, rust_version: str) -> str:
+  if build_type == 'nightly':
     return RUSTC_SOURCE_URL_NIGHTLY
-  elif args.beta:
+  elif build_type == 'beta':
     return RUSTC_SOURCE_URL_BETA
   else:
-    return RUSTC_SOURCE_URL_VERSION_TEMPLATE % args.rust_version
+    return RUSTC_SOURCE_URL_VERSION_TEMPLATE % rust_version
+
+
+def get_extra_tag(build_type: str) -> str:
+  if build_type:
+    return '-' + build_type
+  else:
+    return ''
 
 
 def version_string_type(arg_string: str) -> str:
@@ -56,49 +85,102 @@ def version_string_type(arg_string: str) -> str:
   else:
     raise argparse.ArgumentTypeError("Version string is not properly formatted")
 
-def get_extra_tag(args: argparse.ArgumentParser):
-  if args.nightly:
-    return '-nightly'
-  elif args.beta:
-    return '-beta'
-  else:
-    return ''
+#
+# Git helpers
+#
 
+def git_branch_exists(branch_name: str) -> bool:
+  return exec_rustc_src_command(COMMAND_GIT_TEST % branch_name).returncode == 0
+
+
+def git_get_branch_target(branch_name: str) -> str:
+  return exec_rustc_src_command(COMMAND_GIT_TEST % branch_name, check=True, stdout=subprocess.PIPE).stdout.rstrip()
+
+#
+# Program logic
+#
 
 def parse_args() -> argparse.ArgumentParser:
   parser = argparse.ArgumentParser(description='Fetch and unpack a Rust source archive')
 
   exclusive_group = parser.add_mutually_exclusive_group()
-  exclusive_group.add_argument("-b", "--beta", dest="beta", action="store_true",
-    help="fetch the beta archive")
-  exclusive_group.add_argument("-n", "--nightly", dest="nightly", action="store_true",
-    help="fetch the nightly archive")
+  exclusive_group.add_argument("-b", "--beta", dest="build_type", action="store_const",
+    default='', const='beta', help="fetch the beta archive")
+  exclusive_group.add_argument("-n", "--nightly", dest="build_type", action="store_const",
+    default='', const='nightly', help="fetch the nightly archive")
+  parser.add_argument("-o", "--overwrite", dest="overwrite", action="store_true",
+    help="Overwrite an existing branch if it exists")
 
   parser.add_argument("rust_version", action="store", type=version_string_type)
 
   return parser.parse_args()
 
 
-def main() -> None:
-  args = parse_args()
+def setup_git_branch(branch_name: str, overwrite: bool) -> None:
+  print('')
+  if git_branch_exists(branch_name):
+    global branch_existed
+    branch_existed = True
 
-  print("\nCreating branch")
-  command_repo = COMMAND_REPO % (args.rust_version + get_extra_tag(args))
-  exec_rustc_src_command(command_repo, "Error creating repo for source update")
+    if overwrite:
+      print("Checking out branch %s" % branch_name)
+      handle_rustc_src_command(COMMAND_GIT_CHECKOUT % branch_name,
+                             "Error checking out branch to overwrite")
+    else:
+      print("Branch %s already exists and the 'overwrite' option was not set" % branch_name)
+      exit(-1)
 
+  else:
+    print("Creating branch %s" % branch_name)
+    command_repo = COMMAND_REPO % branch_name
+    handle_rustc_src_command(command_repo, "Error creating repo for source update")
+
+
+def clean_repository() -> None:
   print("Deleting old files")
-  exec_rustc_src_command(COMMAND_GIT_RM, "Error deleting old files from git")
+  handle_rustc_src_command(COMMAND_GIT_RM, "Error deleting old files from git")
 
-  archive_url = construct_archive_url(args)
+
+def fetch_archive(build_type: str, rust_version: str) -> None:
+  archive_url = construct_archive_url(build_type, rust_version)
   print("Fetching archive %s\n" % archive_url)
-  exec_rustc_src_command(
+  handle_rustc_src_command(
     COMMAND_FETCH % archive_url,
-    "Error fetching source for Rust version %s" % args.rust_version,
-    stdout=None)
+    "Error fetching source for Rust version %s" % rust_version,
+    stdout=None,
+    stderr=None)
 
-  print("\nCommiting new files")
-  exec_rustc_src_command(COMMAND_GIT_ADD, "Error adding new files to git")
-  exec_rustc_src_command(COMMAND_GIT_COMMIT % args.rust_version, "Error commiting new files to git")
+
+def commit_files(branch_name: str, rustc_version: str) -> None:
+  global branch_existed
+
+  print()
+  handle_rustc_src_command(COMMAND_GIT_ADD, "Error adding new files to git")
+
+  if exec_rustc_src_command(COMMAND_GIT_DIFF).returncode == 0:
+    print("No update to source")
+    exit(0)
+  elif branch_existed and (git_get_branch_target(branch_name) != git_get_branch_target(GIT_REFERENCE_BRANCH)):
+    print("Amending previous commit")
+    handle_rustc_src_command(COMMAND_GIT_AMEND,
+                             "Error amending previous commit")
+  else:
+    print("Commiting new files")
+    handle_rustc_src_command(COMMAND_GIT_COMMIT % rustc_version,
+                             "Error committing new files to git")
+
+
+def main() -> None:
+  args         = parse_args()
+  rust_version = args.rust_version + get_extra_tag(args.build_type)
+  branch_name  = BRANCH_NAME % rust_version
+
+  setup_git_branch(branch_name, args.overwrite)
+  clean_repository()
+  fetch_archive(args.build_type, rust_version)
+  commit_files(branch_name, rust_version)
+
+  exit(0)
 
 
 if __name__ == '__main__':
